b1af89f296bafc80fd85908343e004c04ae325d1
xkevin190 <kevinvelasco193@gmail.com>
2019-12-12 10:10:06 -0400

    aggregate imports

--- android/src/main/java/com/reactlibrary/LanguageDetectorModule.java
+++ android/src/main/java/com/reactlibrary/LanguageDetectorModule.java
@@ -2,8 +2,9 @@ package com.reactlibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LanguageDetectorModule extends ReactContextBaseJavaModule {
 
